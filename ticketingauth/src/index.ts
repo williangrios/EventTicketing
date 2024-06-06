9a21eddcdb6272d8804c8dc381c05c75fd04280c
Willian Rios <williangrios@yahoo.com.br>
2024-06-06 07:25:39 -0300

    feat: add new feature

--- ticketingauth/src/index.ts
+++ ticketingauth/src/index.ts
@@ -1,14 +1,25 @@
 import express from 'express'
-import { json } from 'body-parser'
+import 'express-async-errors'
+import { currentUserRouter } from './routes/current-user'
+import { signInRouter } from './routes/signin'
+import { signOutRouter } from './routes/signout'
+import { signUpRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import NotFoundError from './errors/not-found-error'
 
 const app = express()
-app.use(json())
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('hi there')
+app.use(express.json())
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(signUpRouter)
+app.all('*', async () => {
+  throw new NotFoundError()
 })
+app.use(errorHandler)
 
 const PORT = 3000
 app.listen(PORT, () => {
-  console.log('TICKETING AUTH SERVICE LISTENING ON PORT ', PORT)
+  console.log('TICKETING AUTH SERVICE LISTENING ON PORT-- ', PORT)
 })
