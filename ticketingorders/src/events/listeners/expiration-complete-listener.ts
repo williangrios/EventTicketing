fcb85a1aa7cd1195e8dade1cccf97a2433b44ac2
Willian Rios <williangrios@yahoo.com.br>
2024-08-12 09:02:29 -0300

    feat: add funtionality

---
+++ ticketingorders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,36 @@
+import {
+  OrderStatus,
+  Subjects,
+  ExpirationCompleteEvent,
+  Listener,
+} from '@wrticketing/commom-v2'
+import { Message } from 'node-nats-streaming'
+import { queueGroupName } from './queue-group-name'
+import { Order } from '../../model/order'
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher'
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
+  queueGroupName = queueGroupName
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket')
+    if (!order) throw new Error('Order not found')
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack
+    }
+    order.set({
+      status: OrderStatus.Cancelled,
+    })
+    await order.save()
+
+    // publicar o evento de order cancelled
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    })
+    msg.ack()
+  }
+}
